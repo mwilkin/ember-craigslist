1e4270905fe653921147c5991c574fa457a7f72f
Matt Wilkin and Ryan Streur <matt+ryan@>
2016-08-22 14:12:55 -0700

    add timestamping functionality to listing creation

--- app/components/new-listing-form.js
+++ app/components/new-listing-form.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     saveListing() {
+      var date = new Date().getTime();
       var params = {
         category: this.get('category'),
         name: this.get('name') ? this.get('name'): "",
@@ -10,7 +11,7 @@ export default Ember.Component.extend({
         imageUrl: this.get('imageUrl') ? this.get('imageUrl'): "",
         description: this.get('description') ? this.get('description'): "",
         price: this.get('price') ? this.get('price'): "",
-        date: this.get('date') ? this.get('date'): ""
+        date
       };
       this.sendAction('saveListingToFirebase', params);
     }
