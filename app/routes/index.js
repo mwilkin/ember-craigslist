1b62fbae4c03d45e6f7210c550b1f8a29feede09
Matt Wilkin and Ryan Streur <matt+ryan@>
2016-08-22 10:47:26 -0700

    add a list of categories with links to each category route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash({
+      categories: this.store.findAll('category')
+    });
+  },
   actions: {
     saveCategoryToFirebase(params) {
       var newCategory = this.store.createRecord('category', params);
